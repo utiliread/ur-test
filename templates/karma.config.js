ad142662acfeec68d990523268478956c001c2b0
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2018-01-24 13:59:27 +0100

    feat(package) add typescript as peer dependency

--- templates/karma.config.js
+++ templates/karma.config.js
@@ -1,32 +1,29 @@
 module.exports = function (config) {
     config.set({
-      frameworks: ['mocha', 'chai', 'karma-typescript'],
-      files: [
-        { pattern: 'src/**/*.ts' }
-      ],
-      preprocessors: {
-        'src/**/*.ts': ['karma-typescript']
-      },
-      reporters: ['progress', 'karma-typescript'],
-  
-      browsers: ['ChromeHeadless', 'ChromeDebugging'],
-  
-      customLaunchers: {
-        ChromeDebugging: {
-          base: 'Chrome',
-          flags: ['--remote-debugging-port=9222']
+        frameworks: ['mocha', 'chai', 'karma-typescript'],
+        files: [
+            'src/**/*.ts'
+        ],
+        preprocessors: {
+            'src/**/*.ts': ['karma-typescript']
+        },
+        reporters: ['progress', 'karma-typescript'],
+        browsers: ['ChromeHeadless', 'ChromeDebugging'],
+        customLaunchers: {
+            ChromeDebugging: {
+                base: 'Chrome',
+                flags: ['--remote-debugging-port=9222']
+            }
+        },
+        karmaTypescriptConfig: {
+            bundlerOptions: {
+                entrypoints: /\.spec\.ts$/,
+                sourceMap: true
+            },
+            coverageOptions: {
+                instrumentation: false
+            },
+            tsconfig: './tsconfig.test.json'
         }
-      },
-  
-      karmaTypescriptConfig: {
-        bundlerOptions: {
-          entrypoints: /\.spec\.ts$/,
-          sourceMap: true
-        },
-        coverageOptions: {
-          instrumentation: false
-        },
-        tsconfig: './tsconfig.test.json'
-      }
     })
-  }
+}
